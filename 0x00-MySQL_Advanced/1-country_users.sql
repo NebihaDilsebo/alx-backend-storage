d0740d009e59149911c4c3b819af496ead04dfd8
Nebiha Dilsebo <shegolenur@hotmail.com>
2023-10-12 10:43:53 +0000

    1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,5 +3,5 @@
 CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	country ENUM('US', 'CO', 'TN')
-	);
+	country ENUM('US', 'CO', 'TN') NOT NULL
+);
