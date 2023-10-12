b7a83a47ee96e3deac37edcd342fbbd344568600
Nebiha Dilsebo <shegolenur@hotmail.com>
2023-10-12 15:40:03 +0000

    3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,11 @@
 -- Lists all bands with Glam as their main style, ranked by their longevity
 
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan
+SELECT band_name, 
+       CASE
+           WHEN split IS NULL THEN 2022 - formed
+           ELSE split - formed
+       END AS lifespan
 FROM metal_bands
-WHERE style like '%Glam rock%';
+WHERE style LIKE '%Glam rock%'
+HAVING lifespan > 0
+ORDER BY lifespan DESC;
