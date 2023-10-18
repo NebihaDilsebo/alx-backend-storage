8a929e518e7c75793515d67f0d31b8d4290bd2c7
NebihaDilsebo <shegolenur@hotmail.com>
2023-10-18 08:13:50 -0700

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,46 +1,36 @@
 #!/usr/bin/env python3
 """
-web file
+Web file
 """
 import requests
 import redis
+from functools import wraps
+
+store = redis.Redis()
 
 
+def count_url_access(method):
+    """ Decorator counting how many times
+    a Url is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
+
+
+@count_url_access
 def get_page(url: str) -> str:
-    # Initialize a Redis client
-    redis_client = redis.Redis()
-
-    # Check if the URL access count is stored in the cache
-    count_key = f"count:{url}"
-    access_count = redis_client.get(count_key)
-
-    if access_count is not None:
-        access_count = int(access_count)
-    else:
-        access_count = 0
-
-    # If the URL was accessed fewer than 10 times, fetch the HTML content
-    if access_count < 10:
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                # Cache the HTML content with a 10-second expiration
-                redis_client.setex(url, 10, response.text)
-
-                # Update the access count and store it in the cache
-                access_count += 1
-                redis_client.set(count_key, access_count)
-
-                return response.text
-            else:
-                return f"Failed to retrieve the page at {url}. Status code: {response.status_code}"
-        except requests.exceptions.RequestException as e:
-            return f"Error: {str(e)}"
-    else:
-        return "Access limit reached for this URL."
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/https://example.com"  # Replace with your desired URL
-    html_content = get_page(url)
-    print(html_content)
-
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
