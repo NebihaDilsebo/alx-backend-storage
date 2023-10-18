86ed267b9521ca4a1819aca245a8692ef69204b6
NebihaDilsebo <shegolenur@hotmail.com>
2023-10-18 08:21:47 -0700

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -34,3 +34,10 @@ def get_page(url: str) -> str:
     """ Returns HTML content of a url """
     res = requests.get(url)
     return res.text
+
+if __name__ == "__main__":
+    # Replace this URL with the one you want to test
+    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/https://example.com"
+
+    html_content = get_page(url)
+    print(html_content)
